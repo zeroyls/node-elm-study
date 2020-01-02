9af64016fed67abfc553920ac7028756693dc296
yelang <125223892@qq.com>
2020-01-02 17:06:09 +0800

    Add login api for admin model

--- controller/admin/admin.js
+++ controller/admin/admin.js
@@ -10,10 +10,7 @@ class Admin extends BaseComponent{
     constructor(){
         super();
         this.register = this.register.bind(this);
-    }
-
-    login(req, res, next){
-
+        this.login = this.login.bind(this);
     }
 
     async register(req, res, next){
@@ -76,6 +73,61 @@ class Admin extends BaseComponent{
                     status: 0,
                     type: 'REGISTER_ADMIN_FAILED',
                     message: '注册管理员失败'
+                })
+            }
+        })
+    }
+    
+    async login(req, res, next){
+        const form = new formidable.IncomingForm();
+        form.parse(req, async(err, fields, files) => {
+            if(err){
+                res.send({
+                    status: 0,
+                    type: 'FORM_DATA_ERROR',
+                    message: '表单信息错误'
+                })
+                return
+            }
+            const {user_name, password, status = 1} = fields;
+            try{
+                if(!user_name){
+                    throw new Error('用户名参数错误');
+                }else if(!password){
+                    throw new Error('密码参数错误');
+                }
+            }catch(err){
+                res.send({
+                    status: 0,
+                    type: 'GET_ERROR_PARAM',
+                    message: err.message
+                })
+                return
+            }
+            const newpassword = this.encryption(password);
+            try{
+                const admin = await AdminModel.findOne({user_name});
+                if(!admin){
+                    res.send({
+                        status: 0,
+                        message: '用户名不存在'
+                    })
+                }else if(newpassword.toString() != admin.password.toString()){
+                    res.send({
+                        status: 0,
+                        message: '管理员登录密码错误'
+                    })
+                }else{
+                    req.session.admin_id = admin.id;
+                    res.send({
+                        status: 1,
+                        message: '登录成功'
+                    })
+                }
+            }catch(err){
+                res.send({
+                    status: 0,
+                    message: '管理员登录失败'
                 })
             }
         })
--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,8 @@ import Admin from '../controller/admin/admin';
 
 const router = express.Router();
 
-router.post('/login', Admin.login);
 router.post('/register', Admin.register);
+router.post('/login', Admin.login);
+
 
 export default router;
