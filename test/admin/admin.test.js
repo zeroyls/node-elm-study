10986fc96c3726fd09827d989b97f6b5de2a2107
yelang <125223892@qq.com>
2020-01-10 16:03:49 +0800

    Refine admin test file

--- test/admin/admin.test.js
+++ test/admin/admin.test.js
@@ -1,6 +1,13 @@
 
 const agent = require('superagent');
 const should = require('should');
+const config = require('config-lite')({
+    filename: 'default',
+    config_basedir: __dirname,
+    config_dir: 'config'
+});
+
+const prefixUrl = 'http://localhost' + config.port;
 
 
 describe('admin controller', function(){
@@ -10,7 +17,7 @@ describe('admin controller', function(){
             password: 'admin'
         };
 
-        const resp = await agent.post('http://localhost:3000/admin/register', params);
+        const resp = await agent.post(prefixUrl + '/admin/register', params);
         should.notEqual(resp.status, 0);
     })
 })
