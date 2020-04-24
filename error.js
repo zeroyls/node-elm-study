9cd860d898d547bf6228b6daaca80663d58b20d0
yelang <125223892@qq.com>
2020-04-24 10:21:07 +0800

    Add error.js

---
+++ error.js
@@ -0,0 +1,31 @@
+export default {
+    'ERROR_OK': 0,
+    'REQUEST_DATA_ERROR': 1000,//请求格式错误
+    'DATABASE_ERROR' : 1001,
+    'DATABASE_ID_ERROR': 1002,
+    'NOT_LOGIN': 1003,
+    
+    'USER_HAS_EXIST' : 2000,
+    'USER_NOT_EXIST' : 2001,
+    'PASSWORD_ERROR' : 2002,
+    'SIGOUT_ERROR': 2004,
+    'GET_ALL_ADMIN_ERROR' :2005,
+    'GET_ADMIN_COUNT_ERROR' :2006,
+    'GET_ADMIN_INFO_ERROR': 2007,
+
+    'UPLOAD_IMG_ERROR' : 3001,//上传图片错误
+    'IMG_FORMAT_ERROR' : 3002,//图片格式错误
+    'SAVE_IMG_ERROR' : 3003,//保存图片失败
+    'PARSE_IMG_ERROR': 3004,//解析图片失败
+    'UNKNOW_ERROR' : -1,
+
+    'CITY_GET_ERROR': 4001
+
+
+
+
+}
+
+
+
+
